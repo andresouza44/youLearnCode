55e704873ebfaa4c022e05ab04a78b741d8c0c17
Andre M. de Souza <andresouza44@gmail.com>
2023-08-25 15:37:17 -0300

    you learn code initial commit

---
+++ spring-boot-rest-controller-tutorial/spring-boot-rest-controller-tutorial/src/test/java/com/codelear/springbootrestcontrollertutorial/SpringBootRestControllerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelear.springbootrestcontrollertutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestControllerTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
